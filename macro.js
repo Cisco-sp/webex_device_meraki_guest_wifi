2884d2166b38f12f59f7887d326e5885e45462fe
Victor Gindro -X (vgindro - CIEE at Cisco) <vgindro@cisco.com>
2019-12-06 11:11:37 -0300

    updated with exception and internal names

--- macro.js
+++ macro.js
@@ -1,21 +1,57 @@
 const xapi = require('xapi');
 var payload;
 
-const MERAKI_KEY = ""; // Fill in Cisco Meraki Dashboard API key
-const MERAKI_URL = "" // Fill in first part of the Meraki Dashboard URL (e.g. https://n60.meraki.com)
-const SSID_NUMBER = "" // Fill in SSID number
-const NETWORK_ID = "" // Fill in network ID
-const SSID_NAME = "EDR-Meraki-Guest" // Fill in SSID name
+const MERAKI_KEY = "74d23386bdfb4ea9f3317b534119783d96ebeee9"; // Fill in Cisco Meraki Dashboard API key
+const MERAKI_URL = "https://n66.meraki.com" // Fill in first part of the Meraki Dashboard URL (e.g. https://n60.meraki.com)
+const SSID_NUMBER = "1" // Fill in SSID number
+const NETWORK_ID = "L_600104650347133228" // Fill in network ID
+const SSID_NAME = "Guest-Wifi-test" // Fill in SSID name
 
 
 function updateMerakiSSID(psk){
   payload = JSON.stringify(psk);
-  console.log(payload);
+  console.log(psk.psk);
   xapi.command('HttpClient Put', {
     Header: ["X-Cisco-Meraki-API-Key: " + MERAKI_KEY, "Content-Type: application/json"],
     Url: MERAKI_URL + "/api/v0/networks/" + NETWORK_ID + "/ssids/" + SSID_NUMBER,
     AllowInsecureHTTPS: 'True'
-  }, payload).catch(e => console.log(JSON.stringify(e)))}
+  }, payload).then((result)=>{
+    if(psk.psk === undefined){
+      xapi.command('UserInterface Message Prompt Display', {
+        Title: 'Guest Wifi',
+        Text: 'Desabilitada!'
+      });
+    }else{
+      xapi.command('UserInterface Message Prompt Display', {
+        Title: SSID_NAME+' Habilitada!',
+        Text: 'senha: '+ psk.psk
+      });
+    }
+
+  }).catch((e)=>{
+/*    if(e.data.StatusCode == 400){
+     xapi.command('UserInterface Message Prompt Display', {
+        Title: 'Guest Wifi',
+        Text: 'Erro: Senha muito pequena, minimo de 8 caracteres'
+      }); 
+    }
+    else{
+      xapi.command('UserInterface Message Prompt Display', {
+        Title: 'Guest Wifi',
+        Text: 'Erro: '+ e.data.StatusCode
+      });
+    }*/
+          xapi.command('UserInterface Message Prompt Display', {
+        Title: 'Guest Wifi',
+        Text: 'Erro: Equipamento desligado '+ e.data.StatusCode
+      });
+    xapi.command("UserInterface Extensions Widget SetValue", {
+      WidgetId: 'wifi',
+      value: 'off'
+    });
+  });
+  
+}
 
 function guiEvent(event) {
   if (event.WidgetId === 'wifi') {
@@ -23,9 +59,10 @@ function guiEvent(event) {
       xapi.command('UserInterface Message TextInput Display', {
        FeedbackId: 'psk',
        Title: SSID_NAME,
-       Text: 'Choose a password (min 8 characters)',
-       Placeholder: 'Type password'
+       Text: 'Escolha uma senha (min 8 characters)',
+       Placeholder: 'Senha'
       });
+      console.log(event);
     } else if (event.Type === 'changed' && event.Value === 'off') {
       var data =
       {
